d4c47801eb23d450356647839fc6fb7e558bfbc4
FelipeHanada <146875733+FelipeHanada@users.noreply.github.com>
2026-04-24 14:52:32 -0300

    feat: add scenes

--- Catan/Source/Scenes/GameScene.cs
+++ Catan/Source/Scenes/GameScene.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 
 namespace Catan.Source.Scenes
 {
     internal class GameScene : Scene
     {
+            public GameScene()
+            {
+            }
+    
+            public override void Initialize()
+            {
+                base.Initialize();
+            }
+    
+            public override void LoadContent()
+            {
+                base.LoadContent();
+            }
+    
+            public override void UnloadContent()
+            {
+                base.UnloadContent();
+            }
+    
+            public override void Update(GameTime gameTime)
+            {
+                base.Update(gameTime);
+            }
+    
+            public override void Draw(GameTime gameTime)
+            {
+                base.Draw(gameTime);
+        }
     }
 }
