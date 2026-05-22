a7baa784b699880784edef4ccb7b78afac1f0adb
FelipeHanada <felipehanada@id.uff.br>
2026-05-22 19:04:02 -0300

    refactor: sound board debug

--- Catan/Source/Scenes/GameScene.cs
+++ Catan/Source/Scenes/GameScene.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Catan.Source.Content;
 using Catan.Source.Game.Board;
-using System.Linq;
 using Catan.Source.Game.Dice;
+using Catan.Source.Game.Debug;
 
 
 namespace Catan.Source.Scenes
@@ -66,10 +67,6 @@ namespace Catan.Source.Scenes
     {
         public override MusicId? Music => MusicId.Partida;
 
-#if DEBUG
-        private SpriteFont _font;
-        private Texture2D _pixel;
-#endif
         private Atlas _atlas;
 
         private Stack<GameState> _stateStack;
@@ -79,21 +76,6 @@ namespace Catan.Source.Scenes
         private MouseState _previousMouseState;
         private TurnPhase _turnPhase;
         private DiceRoll? _lastDiceRoll;
-#if DEBUG
-        private KeyboardState _previousKeyboardState;
-
-        private readonly (Keys PrimaryKey, Keys AlternativeKey, SfxId SoundId, string Label)[] _soundHotkeys =
-        {
-            (Keys.D1, Keys.NumPad1, SfxId.ConstrucaoEstrada, "1 - Estrada"),
-            (Keys.D2, Keys.NumPad2, SfxId.ConstrucaoCasa, "2 - Casa"),
-            (Keys.D3, Keys.NumPad3, SfxId.LadraoDado7, "3 - Ladrao"),
-            (Keys.D4, Keys.NumPad4, SfxId.Ovelha, "4 - Ovelha"),
-            (Keys.D5, Keys.NumPad5, SfxId.Pedra, "5 - Pedra"),
-            (Keys.D6, Keys.NumPad6, SfxId.Planta, "6 - Planta"),
-            (Keys.D7, Keys.NumPad7, SfxId.Tijolo, "7 - Tijolo"),
-            (Keys.D8, Keys.NumPad8, SfxId.TijoloCaindo, "8 - Tijolo caindo"),
-        };
-#endif
 
         public GameScene()
         {
@@ -110,12 +92,10 @@ namespace Catan.Source.Scenes
         public override void LoadContent()
         {
             base.LoadContent();
-#if DEBUG
-            _font = Game1.ContentManager.Load<SpriteFont>("DefaultFont");
 
-            _pixel = new Texture2D(Game1.GraphicsDeviceInstance, 1, 1);
-            _pixel.SetData(new[] { Color.White });
-#endif
+            #if DEBUG
+            Subscribe(new SoundBoardDebug());
+            #endif
 
 
             _atlas = new Atlas(Game1.ContentManager);
@@ -130,9 +110,6 @@ namespace Catan.Source.Scenes
                 viewport.Width - diceControlWidth - 32,
                 viewport.Height - DiceRollControl.FaceSize - 32,
                 _atlas);
-#if DEBUG
-            _previousKeyboardState = Keyboard.GetState();
-#endif
 
             _previousMouseState = Mouse.GetState();
             _turnPhase = TurnPhase.WaitingForDiceRoll;
@@ -141,12 +118,6 @@ namespace Catan.Source.Scenes
 
         public override void UnloadContent()
         {
-#if DEBUG
-            _pixel?.Dispose();
-            _pixel = null;
-            _font = null;
-#endif
-
             base.UnloadContent();
         }
 
@@ -158,21 +129,7 @@ namespace Catan.Source.Scenes
                 return;
             }
 
-#if DEBUG
-            var keyboardState = Keyboard.GetState();
 
-            foreach (var hotkey in _soundHotkeys)
-            {
-                if (IsJustPressed(keyboardState, hotkey.PrimaryKey) ||
-                    IsJustPressed(keyboardState, hotkey.AlternativeKey))
-                {
-                    SoundManager.Instance.Play(hotkey.SoundId);
-                    break;
-                }
-            }
-
-            _previousKeyboardState = keyboardState;
-#endif
 
             GameState currentState = GetCurrentStateGame();
             currentState.Update(gameTime);
@@ -210,10 +167,6 @@ namespace Catan.Source.Scenes
             GameState currentState = GetCurrentStateGame();
             currentState.Draw(gameTime);
 
-#if DEBUG
-            DrawSoundHotkeys(gameTime, spriteBatch);
-#endif
-
             _diceRollControl.Draw(gameTime, spriteBatch);
         }
 
@@ -230,34 +183,6 @@ namespace Catan.Source.Scenes
             _stateStack.Append(gameState);
             gameState.Initialize();
         }
-
-#if DEBUG
-        private void DrawSoundHotkeys(GameTime gameTime, SpriteBatch spriteBatch)
-        {
-            const int x = 880;
-            const int y = 24;
-            const int width = 360;
-            const int padding = 16;
-            const int lineHeight = 26;
-
-            var height = padding * 2 + lineHeight * (_soundHotkeys.Length + 1);
-            var panel = new Rectangle(x, y, width, height);
-
-            spriteBatch.Draw(_pixel, panel, new Color(20, 26, 34, 210));
-            spriteBatch.DrawString(_font, "Teste de som", new Vector2(x + padding, y + padding), Color.White);
-
-            for (var i = 0; i < _soundHotkeys.Length; i++)
-            {
-                var position = new Vector2(x + padding, y + padding + lineHeight * (i + 1));
-                spriteBatch.DrawString(_font, _soundHotkeys[i].Label, position, Color.LightGray);
-            }
-        }
-
-        private bool IsJustPressed(KeyboardState currentState, Keys key)
-        {
-            return currentState.IsKeyDown(key) && !_previousKeyboardState.IsKeyDown(key);
-        }
-#endif
 
         private void ResolveDiceRoll(DiceRoll roll)
         {
