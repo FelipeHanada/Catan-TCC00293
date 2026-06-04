84b289af22fac150f155fb937a2ca2e7c55829bd
FelipeHanada <146875733+FelipeHanada@users.noreply.github.com>
2026-06-04 01:04:32 -0300

    fix: remove obj from scene

--- Catan/Source/Scenes/Scene.cs
+++ Catan/Source/Scenes/Scene.cs
@@ -44,16 +44,6 @@ namespace Catan.Source.Scenes
 
         public virtual void Update(GameTime gameTime)
         {
-            while (ToUnsubscribe.Count > 0)
-            {
-                var obj = ToUnsubscribe.Dequeue();
-                if (GameObjects.Remove(obj))
-                {
-                    obj.OnUnsubscribe(this);
-                }
-            }
-
-
             foreach (var obj in GameObjects)
             {
                 obj.Update(gameTime);
@@ -69,7 +59,14 @@ namespace Catan.Source.Scenes
                 }
             }
 
-
+            while (ToUnsubscribe.Count > 0)
+            {
+                var obj = ToUnsubscribe.Dequeue();
+                if (GameObjects.Remove(obj))
+                {
+                    obj.OnUnsubscribe(this);
+                }
+            }
         }
 
         public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
