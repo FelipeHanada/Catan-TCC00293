ba486df745acfbf1c274393c3f2392a53791e37d
Lucas Avelar <77032122+1lit4@users.noreply.github.com>
2026-06-06 10:47:08 -0300

    feat: hover de carta

--- Catan/Source/Scenes/GameScene/GameStates/Actions/DevelopmentCardState.cs
+++ Catan/Source/Scenes/GameScene/GameStates/Actions/DevelopmentCardState.cs
@@ -6,13 +6,18 @@ using Catan.Source.Game.Player;
 using Catan.Source.Scenes;
 using Catan.Source.Scenes.Game;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
 
 namespace Catan.Source.Scenes.Game
 {
     public class DevelopmentCardState : PlayerTurnGameState, PlayerBuildButtonCallback, PlayerTradeButtonCallback, PlayerDevelopmentCardButtonCallback
 {
-    static UISlate BuildUISlate(GameScene gameScene, Player player)
+    private readonly Dictionary<ButtonAction, DevelopmentCardType> _cardButtons;
+    private readonly DevelopmentCardPreview _preview;
+
+    UISlate BuildUISlate(GameScene gameScene, Player player)
     {
         Atlas atlas = gameScene.Atlas;
         int posX = 760;
@@ -51,6 +56,7 @@ namespace Catan.Source.Scenes.Game
                     player.Inventory.DevelopmentCards,
                     gameScene.HasUsedDevelopmentCardThisTurn));
 
+            _cardButtons[button] = type;
             useSlate.AddChild(button);
             buttonIndex++;
         }
@@ -109,8 +115,24 @@ namespace Catan.Source.Scenes.Game
     public UISlate UISlate { get; private set; }
     public DevelopmentCardState(GameScene gameScene, Player player) : base(gameScene, player)
     {
+        _cardButtons = new Dictionary<ButtonAction, DevelopmentCardType>();
         UISlate = BuildUISlate(gameScene, player);
+        _preview = new DevelopmentCardPreview(613, 390, gameScene.Atlas);
         AddChild(UISlate);
+        AddChild(_preview);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        _preview.CardType = null;
+        foreach (var entry in _cardButtons)
+        {
+            if (entry.Key.IsHovered)
+            {
+                _preview.CardType = entry.Value;
+                return;
+            }
+        }
     }
 
     public void OnPlayerBuildButtonClicked()
@@ -128,6 +150,44 @@ namespace Catan.Source.Scenes.Game
     public void OnPlayerDevelopmentCardButtonClicked()
     {
         _gameScene.ExitState();
+    }
+
+    private class DevelopmentCardPreview : GameObject
+    {
+        private readonly Atlas _atlas;
+        public DevelopmentCardType? CardType { get; set; }
+
+        public DevelopmentCardPreview(float x, float y, Atlas atlas) : base(x, y)
+        {
+            _atlas = atlas;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (!CardType.HasValue)
+            {
+                return;
+            }
+
+            spriteBatch.Draw(
+                _atlas.Texture,
+                new Vector2(X, Y),
+                Atlas.GetRectangle(GetSpriteId(CardType.Value)),
+                Color.White);
+        }
+
+        private static AtlasSpriteId GetSpriteId(DevelopmentCardType type)
+        {
+            return type switch
+            {
+                DevelopmentCardType.Invention => AtlasSpriteId.DevCardInvention,
+                DevelopmentCardType.RoadBuilding => AtlasSpriteId.DevCardRoad,
+                DevelopmentCardType.Monopoly => AtlasSpriteId.DevCardMonopoly,
+                DevelopmentCardType.Knight => AtlasSpriteId.DevCardKnight,
+                DevelopmentCardType.VictoryPoint => AtlasSpriteId.DevCardVP,
+                _ => AtlasSpriteId.DevCardVP,
+            };
+        }
     }
 }
 }
